198eb0a2b41fb45aa64c03ef24a1ccbb187366fa
smokris <smokris@softpixel.com>
2018-03-27 22:16:45 -0400

    Allow linking to libraries in the test_package folder.

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -25,6 +25,6 @@ class GraphvizTestConan(ConanFile):
             self.run('! (otool -L lib/*.dylib | grep -v "^lib/" | egrep -v "^\s*(/usr/lib/|/System/|@rpath/)")')
             self.run('! (otool -l lib/*.dylib | grep -A2 LC_RPATH | cut -d"(" -f1 | grep "\s*path" | egrep -v "^\s*path @(executable|loader)_path")')
         elif platform.system() == 'Linux':
-            self.run('! (ldd lib/*.so | grep -v "^lib/" | grep "/" | egrep -v "\s/lib64/")')
+            self.run('! (ldd lib/*.so | grep -v "^lib/" | grep "/" | egrep -v "(\s(/lib64/|(/usr)?/lib/x86_64-linux-gnu/)|test_package/build)")')
         else:
             raise Exception('Unknown platform "%s"' % platform.system())
