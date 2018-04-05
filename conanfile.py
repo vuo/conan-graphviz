d9227a6c7d06181dd4bea3d364b6938ad6e9031d
Steve Mokris <smokris@softpixel.com>
2018-04-05 17:02:54 -0400

    Update to latest LLVM package and switch to libc++.

--- conanfile.py
+++ conanfile.py
@@ -10,7 +10,7 @@ class GraphvizConan(ConanFile):
     package_version = '3'
     version = '%s-%s' % (source_version, package_version)
 
-    requires = 'llvm/3.3-2@vuo/stable', \
+    build_requires = 'llvm/3.3-5@vuo/stable', \
         'vuoutils/1.0@vuo/stable'
     settings = 'os', 'compiler', 'build_type', 'arch'
     url = 'https://github.com/vuo/conan-graphviz'
@@ -54,7 +54,7 @@ class GraphvizConan(ConanFile):
 
             # The LLVM/Clang libs get automatically added by the `requires` line,
             # but this package doesn't need to link with them.
-            autotools.libs = []
+            autotools.libs = ['c++abi']
 
             autotools.flags.append('-Oz')
             if platform.system() == 'Darwin':
